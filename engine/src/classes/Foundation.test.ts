7b60dee92cee865b55443f18ef6280107ac20d03
Kory <kory.porter@hotmail.com>
2019-10-26 09:41:35 +1100

    Added more tests to Foundation

--- engine/src/classes/Foundation.test.ts
+++ engine/src/classes/Foundation.test.ts
@@ -1,40 +1,305 @@
 import test from "ava";
 import { Card } from "./Card";
-import { Foundation } from "./Foundation";
+import { Foundation, ISerializedFoundation } from "./Foundation";
 
-// test all of the foundation piles here
-
-// HeartsFoundationPile validations under here
-test.only("can only add incrementing hearts to the HeartsFoundationPile", t => {
+test("removeCard() removes the card from the right pile", t => {
   const foundation = new Foundation();
-  let cardAdded = foundation.addCard(new Card("Hearts", "7", false));
-  t.false(cardAdded);
-  cardAdded = foundation.addCard(new Card("Hearts", "Ace", false));
-  t.true(cardAdded);
-  cardAdded = foundation.addCard(new Card("Hearts", "2", false));
-  t.true(cardAdded);
-  cardAdded = foundation.addCard(new Card("Hearts", "3", false));
-  t.true(cardAdded);
-  cardAdded = foundation.addCard(new Card("Spades", "4", false));
-  t.false(cardAdded);
-});
-
-test.only("can remove cards from the HeartsFoundationPile", t => {
-  const foundation = new Foundation();
+  foundation.addCard(new Card("Clubs", "Ace", false));
+  foundation.addCard(new Card("Clubs", "2", false));
+  foundation.addCard(new Card("Clubs", "3", false));
   foundation.addCard(new Card("Hearts", "Ace", false));
   foundation.addCard(new Card("Hearts", "2", false));
-  t.is(foundation.getPileForSuit("Hearts").getCards().length, 2);
-  let cardRemoved = foundation.removeCard(new Card("Hearts", "2", false));
-  t.true(cardRemoved);
-  t.is(foundation.getPileForSuit("Hearts").getCards().length, 1);
+  foundation.addCard(new Card("Hearts", "3", false));
+  foundation.addCard(new Card("Spades", "Ace", false));
+  foundation.addCard(new Card("Spades", "2", false));
+  foundation.addCard(new Card("Spades", "3", false));
+  foundation.addCard(new Card("Diamonds", "Ace", false));
+  foundation.addCard(new Card("Diamonds", "2", false));
+  foundation.addCard(new Card("Diamonds", "3", false));
+
+  foundation.removeCard(new Card("Diamonds", "3", false));
+
+  t.is(foundation.getPileForSuit("Diamonds").getCards().length, 2);
+  t.is(foundation.getPileForSuit("Hearts").getCards().length, 3);
+  t.is(foundation.getPileForSuit("Spades").getCards().length, 3);
+  t.is(foundation.getPileForSuit("Clubs").getCards().length, 3);
+  foundation.removeCard(new Card("Diamonds", "2", false));
+  t.is(foundation.getPileForSuit("Diamonds").getCards().length, 1);
+  t.is(foundation.getPileForSuit("Hearts").getCards().length, 3);
+  t.is(foundation.getPileForSuit("Spades").getCards().length, 3);
+  t.is(foundation.getPileForSuit("Clubs").getCards().length, 3);
 });
 
-test.only("cannot remove a card from the HeartsFoundationPile if there are no cards in the pile", t => {
+test("addCard() adds the card to the right pile", t => {
   const foundation = new Foundation();
-  let cardRemoved = foundation.removeCard(new Card("Hearts", "7", false));
-  t.false(cardRemoved);
+  foundation.addCard(new Card("Clubs", "Ace", false));
+  foundation.addCard(new Card("Clubs", "2", false));
+  foundation.addCard(new Card("Clubs", "3", false));
+
+  t.is(foundation.getPileForSuit("Clubs").getCards().length, 3);
+  t.is(foundation.getPileForSuit("Diamonds").getCards().length, 0);
+  t.is(foundation.getPileForSuit("Hearts").getCards().length, 0);
+  t.is(foundation.getPileForSuit("Spades").getCards().length, 0);
 });
 
-// test the foundation class here
+test("serialize() returns the expected data", t => {
+  const foundation = new Foundation();
+  foundation.addCard(new Card("Clubs", "Ace", true));
+  foundation.addCard(new Card("Clubs", "2", true));
+  foundation.addCard(new Card("Clubs", "3", true));
+  foundation.addCard(new Card("Hearts", "Ace", true));
+  foundation.addCard(new Card("Hearts", "2", true));
+  foundation.addCard(new Card("Hearts", "3", true));
+  foundation.addCard(new Card("Spades", "Ace", true));
+  foundation.addCard(new Card("Spades", "2", true));
+  foundation.addCard(new Card("Spades", "3", true));
+  foundation.addCard(new Card("Diamonds", "Ace", true));
+  foundation.addCard(new Card("Diamonds", "2", true));
+  foundation.addCard(new Card("Diamonds", "3", true));
+  const serializedFoundation = foundation.serialize();
+  t.deepEqual(serializedFoundation, {
+    clubs: {
+      cards: [
+        {
+          rank: "Ace",
+          suit: "Clubs",
+          upturned: true
+        },
+        {
+          rank: "2",
+          suit: "Clubs",
+          upturned: true
+        },
+        {
+          rank: "3",
+          suit: "Clubs",
+          upturned: true
+        }
+      ],
+      suit: "Clubs"
+    },
+    diamonds: {
+      cards: [
+        {
+          rank: "Ace",
+          suit: "Diamonds",
+          upturned: true
+        },
+        {
+          rank: "2",
+          suit: "Diamonds",
+          upturned: true
+        },
+        {
+          rank: "3",
+          suit: "Diamonds",
+          upturned: true
+        }
+      ],
+      suit: "Diamonds"
+    },
+    hearts: {
+      cards: [
+        {
+          rank: "Ace",
+          suit: "Hearts",
+          upturned: true
+        },
+        {
+          rank: "2",
+          suit: "Hearts",
+          upturned: true
+        },
+        {
+          rank: "3",
+          suit: "Hearts",
+          upturned: true
+        }
+      ],
+      suit: "Hearts"
+    },
+    spades: {
+      cards: [
+        {
+          rank: "Ace",
+          suit: "Spades",
+          upturned: true
+        },
+        {
+          rank: "2",
+          suit: "Spades",
+          upturned: true
+        },
+        {
+          rank: "3",
+          suit: "Spades",
+          upturned: true
+        }
+      ],
+      suit: "Spades"
+    }
+  });
+});
 
-test("removeCard() actually removes a card from the pile", t => {});
+test("unserialize() returns a Foundation class with the correct data", t => {
+  const unserializedFoundation: ISerializedFoundation = {
+    clubs: {
+      cards: [
+        {
+          rank: "Ace",
+          suit: "Clubs",
+          upturned: true
+        },
+        {
+          rank: "2",
+          suit: "Clubs",
+          upturned: true
+        },
+        {
+          rank: "3",
+          suit: "Clubs",
+          upturned: true
+        }
+      ],
+      suit: "Clubs"
+    },
+    diamonds: {
+      cards: [
+        {
+          rank: "Ace",
+          suit: "Diamonds",
+          upturned: true
+        },
+        {
+          rank: "2",
+          suit: "Diamonds",
+          upturned: true
+        },
+        {
+          rank: "3",
+          suit: "Diamonds",
+          upturned: true
+        }
+      ],
+      suit: "Diamonds"
+    },
+    hearts: {
+      cards: [
+        {
+          rank: "Ace",
+          suit: "Hearts",
+          upturned: true
+        },
+        {
+          rank: "2",
+          suit: "Hearts",
+          upturned: true
+        },
+        {
+          rank: "3",
+          suit: "Hearts",
+          upturned: true
+        }
+      ],
+      suit: "Hearts"
+    },
+    spades: {
+      cards: [
+        {
+          rank: "Ace",
+          suit: "Spades",
+          upturned: true
+        },
+        {
+          rank: "2",
+          suit: "Spades",
+          upturned: true
+        },
+        {
+          rank: "3",
+          suit: "Spades",
+          upturned: true
+        }
+      ],
+      suit: "Spades"
+    }
+  };
+
+  const foundation = Foundation.unserialize(unserializedFoundation);
+  const clubsFoundationPile = foundation.getPileForSuit("Clubs");
+  const heartsFoundationPile = foundation.getPileForSuit("Hearts");
+  const diamondsFoundationPile = foundation.getPileForSuit("Diamonds");
+  const spadesFoundationPile = foundation.getPileForSuit("Spades");
+
+  // clubs
+  t.is(clubsFoundationPile.getCards().length, 3);
+  t.deepEqual(clubsFoundationPile.serialize().cards[0], {
+    rank: "Ace",
+    suit: "Clubs",
+    upturned: true
+  });
+  t.deepEqual(clubsFoundationPile.serialize().cards[1], {
+    rank: "2",
+    suit: "Clubs",
+    upturned: true
+  });
+  t.deepEqual(clubsFoundationPile.serialize().cards[2], {
+    rank: "3",
+    suit: "Clubs",
+    upturned: true
+  });
+
+  // hearts
+  t.is(heartsFoundationPile.getCards().length, 3);
+  t.deepEqual(heartsFoundationPile.serialize().cards[0], {
+    rank: "Ace",
+    suit: "Hearts",
+    upturned: true
+  });
+  t.deepEqual(heartsFoundationPile.serialize().cards[1], {
+    rank: "2",
+    suit: "Hearts",
+    upturned: true
+  });
+  t.deepEqual(heartsFoundationPile.serialize().cards[2], {
+    rank: "3",
+    suit: "Hearts",
+    upturned: true
+  });
+
+  // spades
+  t.is(spadesFoundationPile.getCards().length, 3);
+  t.deepEqual(spadesFoundationPile.serialize().cards[0], {
+    rank: "Ace",
+    suit: "Spades",
+    upturned: true
+  });
+  t.deepEqual(spadesFoundationPile.serialize().cards[1], {
+    rank: "2",
+    suit: "Spades",
+    upturned: true
+  });
+  t.deepEqual(spadesFoundationPile.serialize().cards[2], {
+    rank: "3",
+    suit: "Spades",
+    upturned: true
+  });
+
+  // diamonds
+  t.is(diamondsFoundationPile.getCards().length, 3);
+  t.deepEqual(diamondsFoundationPile.serialize().cards[0], {
+    rank: "Ace",
+    suit: "Diamonds",
+    upturned: true
+  });
+  t.deepEqual(diamondsFoundationPile.serialize().cards[1], {
+    rank: "2",
+    suit: "Diamonds",
+    upturned: true
+  });
+  t.deepEqual(diamondsFoundationPile.serialize().cards[2], {
+    rank: "3",
+    suit: "Diamonds",
+    upturned: true
+  });
+});
--- engine/src/classes/Foundation.ts
+++ engine/src/classes/Foundation.ts
@@ -1,153 +1,55 @@
 import { ISerializable } from "../types/ISerializable";
 import { Pile, ISerializedPile } from "./Pile";
 import { Card } from "./Card";
-import { getNextRankForCurrentRank } from "../helpers/getNextRankForRank";
 import { TSuit } from "../types/TSuit";
-
-export class HeartsFoundationPile extends Pile {
-  constructor(cards: Card[]) {
-    super(cards);
-  }
-
-  canAddCards(cards: Card[]) {
-    const cardsInPile = this.getCards();
-    const cardOnTopOfStack = cardsInPile[cardsInPile.length - 1];
-    let lastCardInLoop = cardOnTopOfStack;
-
-    // early exit if not all same suit, not performant but easy to read!
-    const allCardsAreHearts = cards.every(c => c.getSuit() === "Hearts");
-    if (!allCardsAreHearts) {
-      return false;
-    }
-
-    // now that we know each card is the same suit, we only need to make sure that they are incrementing
-    for (const card of cards) {
-      // the foundation pile for this suit is empty,
-      if (typeof lastCardInLoop === "undefined") {
-        // first card must be an Ace
-        if (card.getRank() === "Ace") {
-          continue;
-        }
-        // if not, return false for this validation function
-        return false;
-      }
-      const nextRank = getNextRankForCurrentRank(lastCardInLoop.getRank());
-      if (card.getRank() !== nextRank) {
-        // break out of the loop early here as we cannot add all the cards!
-        return false;
-      }
-      lastCardInLoop = card;
-    }
-
-    return true;
-  }
-
-  canRemoveCards(amount: number) {
-    const cardsInPile = this.getCards();
-
-    if (amount > cardsInPile.length) {
-      console.warn(
-        "[HeartsFoundationPile] Cannot remove more cards than what currently exists in the pile"
-      );
-      return false;
-    }
-
-    return true;
-  }
-}
-
-export class SpadesFoundationPile extends Pile {
-  constructor(cards: Card[]) {
-    super(cards);
-  }
-
-  canAddCards(cards: Card[]) {
-    return false;
-  }
-
-  canRemoveCards(amount: number) {
-    return false;
-  }
-}
-
-export class ClubsFoundationPile extends Pile {
-  constructor(cards: Card[]) {
-    super(cards);
-  }
-
-  canAddCards(cards: Card[]) {
-    return false;
-  }
-
-  canRemoveCards(amount: number) {
-    return false;
-  }
-}
-
-export class DiamondsFoundationPile extends Pile {
-  constructor(cards: Card[]) {
-    super(cards);
-  }
-
-  canAddCards(cards: Card[]) {
-    return false;
-  }
-
-  canRemoveCards(amount: number) {
-    return false;
-  }
-}
+import { FoundationPile, ISerializedFoundationPile } from "./FoundationPile";
 
 export interface ISerializedFoundation {
-  hearts: ISerializedPile;
-  spades: ISerializedPile;
-  diamonds: ISerializedPile;
-  clubs: ISerializedPile;
+  hearts: ISerializedFoundationPile;
+  spades: ISerializedFoundationPile;
+  diamonds: ISerializedFoundationPile;
+  clubs: ISerializedFoundationPile;
 }
 
 export interface IFoundation
   extends ISerializable<Foundation, ISerializedFoundation> {
-  addCard(card: Card): boolean;
-  removeCard(card: Card): boolean;
+  addCard(card: Card): void;
+  removeCard(card: Card): void;
   getPileForSuit(suit: TSuit): Pile;
 }
 
 export class Foundation implements IFoundation {
-  private hearts: HeartsFoundationPile = new HeartsFoundationPile([]);
-  private spades: SpadesFoundationPile = new SpadesFoundationPile([]);
-  private diamonds: DiamondsFoundationPile = new DiamondsFoundationPile([]);
-  private clubs: ClubsFoundationPile = new ClubsFoundationPile([]);
+  private hearts: FoundationPile = new FoundationPile("Hearts");
+  private spades: FoundationPile = new FoundationPile("Spades");
+  private diamonds: FoundationPile = new FoundationPile("Diamonds");
+  private clubs: FoundationPile = new FoundationPile("Clubs");
 
   getPileForSuit(suit: TSuit): Pile {
     const suitFoundationPileClass = this[suit.toLowerCase()] as Pile;
     return suitFoundationPileClass;
   }
 
-  addCard(card: Card): boolean {
+  addCard(card: Card): void {
     const suit = card.getSuit().toLowerCase();
     const suitFoundationPileClass = this[suit] as Pile;
 
     if (!suitFoundationPileClass.canAddCards([card])) {
-      return false;
+      return;
     }
 
     suitFoundationPileClass.addCards([card]);
-
-    return true;
   }
 
-  removeCard(card: Card): boolean {
+  removeCard(card: Card): void {
     const suit = card.getSuit().toLowerCase();
 
     const suitFoundationPileClass = this[suit] as Pile;
 
-    if (!suitFoundationPileClass.canRemoveCards(1)) {
-      return false;
+    if (!suitFoundationPileClass.canRemoveCards([card])) {
+      return;
     }
 
-    suitFoundationPileClass.removeCards(1);
-
-    return true;
+    suitFoundationPileClass.removeCards([card]);
   }
 
   serialize() {
@@ -160,12 +62,14 @@ export class Foundation implements IFoundation {
   }
 
   static unserialize(data: ISerializedFoundation) {
-    const x = new Foundation();
-    x.clubs = Pile.unserialize(data.clubs);
-    x.diamonds = Pile.unserialize(data.diamonds);
-    x.hearts = Pile.unserialize(data.hearts);
-    x.spades = Pile.unserialize(data.spades);
+    const foundation = new Foundation();
+    Object.keys(data).forEach((suit: TSuit) => {
+      foundation[suit] = FoundationPile.unserialize({
+        cards: data[suit].cards,
+        suit: suit
+      });
+    });
 
-    return x;
+    return foundation;
   }
 }
