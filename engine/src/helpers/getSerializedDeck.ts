a31acf4bad12c324e95668b3f0486a2c37a36d6e
Kory <kory.porter@hotmail.com>
2019-10-26 08:37:23 +1100

    Added helper to get a serialized deck

---
+++ engine/src/helpers/getSerializedDeck.ts
@@ -0,0 +1,62 @@
+import { ISerializedCard } from "../classes/Card";
+
+// TODO - test
+/**
+ * Returns all card in a deck in their serialized form
+ */
+export const getSerializedDeck = (): ISerializedCard[] => {
+  return [
+    { suit: "Clubs", rank: "Ace", upturned: false },
+    { suit: "Clubs", rank: "2", upturned: false },
+    { suit: "Clubs", rank: "3", upturned: false },
+    { suit: "Clubs", rank: "4", upturned: false },
+    { suit: "Clubs", rank: "5", upturned: false },
+    { suit: "Clubs", rank: "6", upturned: false },
+    { suit: "Clubs", rank: "7", upturned: false },
+    { suit: "Clubs", rank: "8", upturned: false },
+    { suit: "Clubs", rank: "9", upturned: false },
+    { suit: "Clubs", rank: "10", upturned: false },
+    { suit: "Clubs", rank: "Jack", upturned: false },
+    { suit: "Clubs", rank: "Queen", upturned: false },
+    { suit: "Clubs", rank: "King", upturned: false },
+    { suit: "Hearts", rank: "Ace", upturned: false },
+    { suit: "Hearts", rank: "2", upturned: false },
+    { suit: "Hearts", rank: "3", upturned: false },
+    { suit: "Hearts", rank: "4", upturned: false },
+    { suit: "Hearts", rank: "5", upturned: false },
+    { suit: "Hearts", rank: "6", upturned: false },
+    { suit: "Hearts", rank: "7", upturned: false },
+    { suit: "Hearts", rank: "8", upturned: false },
+    { suit: "Hearts", rank: "9", upturned: false },
+    { suit: "Hearts", rank: "10", upturned: false },
+    { suit: "Hearts", rank: "Jack", upturned: false },
+    { suit: "Hearts", rank: "Queen", upturned: false },
+    { suit: "Hearts", rank: "King", upturned: false },
+    { suit: "Diamonds", rank: "Ace", upturned: false },
+    { suit: "Diamonds", rank: "2", upturned: false },
+    { suit: "Diamonds", rank: "3", upturned: false },
+    { suit: "Diamonds", rank: "4", upturned: false },
+    { suit: "Diamonds", rank: "5", upturned: false },
+    { suit: "Diamonds", rank: "6", upturned: false },
+    { suit: "Diamonds", rank: "7", upturned: false },
+    { suit: "Diamonds", rank: "8", upturned: false },
+    { suit: "Diamonds", rank: "9", upturned: false },
+    { suit: "Diamonds", rank: "10", upturned: false },
+    { suit: "Diamonds", rank: "Jack", upturned: false },
+    { suit: "Diamonds", rank: "Queen", upturned: false },
+    { suit: "Diamonds", rank: "King", upturned: false },
+    { suit: "Spades", rank: "Ace", upturned: false },
+    { suit: "Spades", rank: "2", upturned: false },
+    { suit: "Spades", rank: "3", upturned: false },
+    { suit: "Spades", rank: "4", upturned: false },
+    { suit: "Spades", rank: "5", upturned: false },
+    { suit: "Spades", rank: "6", upturned: false },
+    { suit: "Spades", rank: "7", upturned: false },
+    { suit: "Spades", rank: "8", upturned: false },
+    { suit: "Spades", rank: "9", upturned: false },
+    { suit: "Spades", rank: "10", upturned: false },
+    { suit: "Spades", rank: "Jack", upturned: false },
+    { suit: "Spades", rank: "Queen", upturned: false },
+    { suit: "Spades", rank: "King", upturned: false }
+  ];
+};
