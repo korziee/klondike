2396f0f8c62270ab1fa4a4cf3616c1be187c1b18
Kory <kory.porter@hotmail.com>
2019-10-29 19:18:21 +1100

    Added stock tests

--- engine/src/classes/Stock.test.ts
+++ engine/src/classes/Stock.test.ts
@@ -1,10 +1,50 @@
 import test from "ava";
 import { Stock } from "./Stock";
+import { Card } from "./Card";
+import { getRandomSuit } from "../helpers/getRandomSuit";
+import { getRandomRank } from "../helpers/getRandomRank";
 
-test("the constructor throws if all cards are not turned down", t => {});
+const getRandomCards = (amount: number): Card[] => {
+  const cards: Card[] = [];
+  for (let i = 0; i < amount; i += 1) {
+    const randomSuit = getRandomSuit();
+    const randomRank = getRandomRank();
+    cards.push(new Card(randomSuit, randomRank, false));
+  }
 
-test("cards cannot be added, an error is thrown", t => {});
+  return cards;
+};
 
-test("cards can be set", t => {});
+test("the constructor throws if all cards are not turned down", t => {
+  const cards = getRandomCards(10);
+  cards[1].turnUp();
+  t.throws(() => {
+    const stock = new Stock(cards);
+  });
 
-test("cards can only be removed one at a time", t => {});
+  cards[1].turnDown();
+
+  t.notThrows(() => {
+    const stock = new Stock(cards);
+  });
+});
+
+test("cards cannot be added, an error is thrown", t => {
+  const cards = getRandomCards(10);
+  const stock = new Stock(cards);
+
+  t.throws(() => {
+    stock.addCards([cards[2], cards[6]]);
+  });
+});
+
+test("cards can be set", t => {
+  const cards = getRandomCards(10);
+  const stock = new Stock([]);
+
+  t.is(stock.getCards().length, 0);
+
+  stock.setCards(cards);
+
+  t.deepEqual(stock.getCards(), cards);
+});
--- engine/src/classes/Stock.ts
+++ engine/src/classes/Stock.ts
@@ -2,6 +2,32 @@ import { Pile } from "./Pile";
 import { Card } from "./Card";
 
 export class Stock extends Pile {
-  canAddCards(cards: Card[]) {}
-  canRemoveCards(cards: Card[]) {}
+  static validateCards(cards: Card[]): void {
+    const allAreUpturned = cards.every(c => c.getUpturned() === false);
+    if (!allAreUpturned) {
+      throw new Error(
+        "All cards in the Stock must be turned down before adding!"
+      );
+    }
+  }
+  constructor(cards: Card[]) {
+    Stock.validateCards(cards);
+    super(cards);
+  }
+
+  canAddCards(cards: Card[]): boolean {
+    throw new Error(
+      "Cannot add cards to the stock, they must be set using `setCards`"
+    );
+  }
+
+  canRemoveCards(cards: Card[]) {
+    // HACK - replace with actual logic
+    return true;
+  }
+
+  canSetCards(cards: Card[]) {
+    Stock.validateCards(cards);
+    return true;
+  }
 }
