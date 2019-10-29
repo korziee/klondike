50edf9e754d00ee322c33b19edf48f70936cc34d
Kory <kory.porter@hotmail.com>
2019-10-29 20:30:56 +1100

    Added back interface for the game

--- engine/src/classes/KlondikeGame.ts
+++ engine/src/classes/KlondikeGame.ts
@@ -1,31 +1,39 @@
 import { ISerializable } from "../types/ISerializable";
-import { Pile, ISerializedPile } from "./Pile";
 import { Card } from "./Card";
+import { ISerializedFoundation } from "./Foundation";
+import { ISerializedTableau } from "./Tableau";
+import { ISerializedWaste } from "./Waste";
+import { ISerializedStock } from "./Stock";
 
-// type GameLocation = "tableau" | "foundation" | "waste" | "stock";
+type GameLocation = "tableau" | "foundation" | "waste" | "stock";
 
-// // describes moving a card
-// export interface IMove {
-//   from: GameLocation;
-//   // you can never move cards to the stock!
-//   to: Omit<GameLocation, "stock">;
-//   cards: Card[];
-//   meta?: {
-//     // used when moving cards from/to/between the tableua
-//     pile?: number;
-//   };
-// }
+// describes moving a card
+export interface IMove {
+  from: GameLocation;
+  // you can never move cards to the stock!
+  to: Omit<GameLocation, "stock">;
+  cards: Card[];
+  meta?: {
+    // used when moving cards from/to/between the tableua
+    pile?: number;
+  };
+}
 
-// export interface ISerializedKlondikeGame {
-//   history: IMove[];
-// }
+export interface ISerializedKlondikeGame {
+  history: IMove[];
+  tableau: ISerializedTableau;
+  foundation: ISerializedFoundation;
+  waste: ISerializedWaste;
+  stock: ISerializedStock;
+  // probably score and other metrics when they are added!
+}
 
-// export interface IKlondikeGame
-//   extends ISerializable<KlondikeGame, ISerializedKlondikeGame> {
-//   getHistory(): IMove[];
-//   getHint(): IMove | null;
-// }
+export interface IKlondikeGame
+  extends ISerializable<KlondikeGame, ISerializedKlondikeGame> {
+  getHistory(): IMove[];
+  getHint(): IMove | null;
+  validateMove(move: IMove): boolean;
+  makeMove(move: IMove): void;
+}
 
-// export class KlondikeGame implements IKlondikeGame {}
-
-// export interface IStock extends ISerializable<Stock, ISerializedPile> {}
+export class KlondikeGame implements IKlondikeGame {}
