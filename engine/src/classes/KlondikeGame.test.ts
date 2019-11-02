f7f96d3a9ce1a74623552649543e8a15c050f49d
Kory <kory.porter@hotmail.com>
2019-11-02 13:19:19 +1100

    Added an unserializing to the Klondike game

--- engine/src/classes/KlondikeGame.test.ts
+++ engine/src/classes/KlondikeGame.test.ts
@@ -1,12 +1,9 @@
 import test from "ava";
-import { KlondikeGame } from "./KlondikeGame";
+import { KlondikeGame, ISerializedKlondikeGame } from "./KlondikeGame";
+import { Card } from "./Card";
 
-test("getHistory() returns the correct history", t => {
-  const game = new KlondikeGame();
-});
-test("history is modified on every successful move", t => {
-  const game = new KlondikeGame();
-});
+test("getHistory() returns the correct history", t => {});
+test("history is modified on every successful move", t => {});
 test("getHistory() returns the correct history in order", t => {});
 test("getHints() returns the correct hints if they exist", t => {});
 test("getHints() returns null if no hints exist", t => {});
@@ -54,5 +51,289 @@ test("can move multiple cards from the tableau to the tableau", t => {});
 test("can move a card from the foundation to the tableau", t => {});
 
 // unserialize and serialize
-test("unserializing the saved game state, puts it back in the same state", t => {});
-test("serializing returns the anticipated state", t => {});
+test("unserializing the saved game state, puts it back in the same state", t => {
+  const serializedGame: ISerializedKlondikeGame = {
+    foundation: {
+      clubs: {
+        suit: "Clubs",
+        cards: [
+          { suit: "Clubs", rank: "Ace", upturned: false },
+          { suit: "Clubs", rank: "2", upturned: false },
+          { suit: "Clubs", rank: "3", upturned: false }
+        ]
+      },
+      hearts: {
+        suit: "Hearts",
+        cards: [
+          { suit: "Hearts", rank: "Ace", upturned: false },
+          { suit: "Hearts", rank: "2", upturned: false },
+          { suit: "Hearts", rank: "3", upturned: false },
+          { suit: "Hearts", rank: "4", upturned: false },
+          { suit: "Hearts", rank: "5", upturned: false }
+        ]
+      },
+      diamonds: {
+        suit: "Diamonds",
+        cards: [
+          { suit: "Diamonds", rank: "Ace", upturned: false },
+          { suit: "Diamonds", rank: "2", upturned: false },
+          { suit: "Diamonds", rank: "3", upturned: false },
+          { suit: "Diamonds", rank: "4", upturned: false },
+          { suit: "Diamonds", rank: "5", upturned: false },
+          { suit: "Diamonds", rank: "6", upturned: false },
+          { suit: "Diamonds", rank: "7", upturned: false },
+          { suit: "Diamonds", rank: "8", upturned: false }
+        ]
+      },
+      spades: {
+        suit: "Spades",
+        cards: [
+          { suit: "Spades", rank: "Ace", upturned: false },
+          { suit: "Spades", rank: "2", upturned: false }
+        ]
+      }
+    },
+    stock: {
+      cards: [
+        {
+          suit: "Spades",
+          rank: "10",
+          upturned: false
+        },
+        {
+          suit: "Spades",
+          rank: "6",
+          upturned: false
+        },
+        {
+          suit: "Spades",
+          rank: "7",
+          upturned: false
+        },
+        {
+          suit: "Spades",
+          rank: "8",
+          upturned: false
+        },
+        {
+          suit: "Clubs",
+          rank: "6",
+          upturned: false
+        },
+        {
+          suit: "Clubs",
+          rank: "Jack",
+          upturned: false
+        },
+        {
+          suit: "Hearts",
+          rank: "8",
+          upturned: false
+        },
+        {
+          suit: "Spades",
+          rank: "4",
+          upturned: false
+        },
+        {
+          suit: "Clubs",
+          rank: "8",
+          upturned: false
+        },
+        {
+          suit: "Clubs",
+          rank: "5",
+          upturned: false
+        },
+        {
+          suit: "Spades",
+          rank: "5",
+          upturned: false
+        },
+        {
+          suit: "Spades",
+          rank: "Queen",
+          upturned: false
+        },
+        {
+          suit: "Hearts",
+          rank: "Queen",
+          upturned: false
+        }
+      ]
+    },
+    history: [],
+    waste: {
+      cards: [
+        {
+          suit: "Diamonds",
+          rank: "King",
+          upturned: true
+        },
+        {
+          suit: "Hearts",
+          rank: "7",
+          upturned: true
+        },
+        {
+          suit: "Spades",
+          rank: "3",
+          upturned: true
+        },
+        {
+          suit: "Spades",
+          rank: "Jack",
+          upturned: true
+        }
+      ]
+    },
+    tableau: {
+      piles: [
+        // empty pile
+        { cards: [] },
+        {
+          cards: [
+            {
+              suit: "Diamonds",
+              rank: "9",
+              upturned: false
+            },
+            {
+              suit: "Clubs",
+              rank: "9",
+              upturned: true
+            }
+          ]
+        },
+        // empty pile
+        { cards: [] },
+        {
+          cards: [
+            {
+              suit: "Spades",
+              rank: "9",
+              upturned: false
+            },
+            {
+              suit: "Hearts",
+              rank: "Jack",
+              upturned: false
+            },
+            {
+              suit: "Diamonds",
+              rank: "10",
+              upturned: false
+            },
+            {
+              suit: "Clubs",
+              rank: "4",
+              upturned: true
+            }
+          ]
+        },
+        {
+          cards: [
+            {
+              suit: "Spades",
+              rank: "King",
+              upturned: false
+            },
+            {
+              suit: "Hearts",
+              rank: "10",
+              upturned: false
+            },
+            {
+              suit: "Hearts",
+              rank: "King",
+              upturned: true
+            }
+          ]
+        },
+        {
+          cards: [
+            {
+              suit: "Clubs",
+              rank: "Queen",
+              upturned: false
+            },
+            {
+              suit: "Clubs",
+              rank: "10",
+              upturned: false
+            },
+            {
+              suit: "Diamonds",
+              rank: "Jack",
+              upturned: true
+            }
+          ]
+        },
+        {
+          cards: [
+            {
+              suit: "Hearts",
+              rank: "6",
+              upturned: false
+            },
+            {
+              suit: "Diamonds",
+              rank: "Queen",
+              upturned: false
+            },
+            {
+              suit: "Hearts",
+              rank: "9",
+              upturned: false
+            },
+            {
+              suit: "Clubs",
+              rank: "7",
+              upturned: false
+            },
+            {
+              suit: "Clubs",
+              rank: "King",
+              upturned: true
+            }
+          ]
+        }
+      ]
+    }
+  };
+
+  const game = KlondikeGame.unserialize(serializedGame);
+
+  // validate the waste is correct an in order!
+  game.waste.serialize().cards.forEach((card, i) => {
+    t.deepEqual(card, serializedGame.waste.cards[i]);
+  });
+
+  // validate the stock
+  game.stock.serialize().cards.forEach((card, i) => {
+    t.deepEqual(card, serializedGame.stock.cards[i]);
+  });
+
+  // validate the tableau
+  game.tableau.serialize().piles.forEach((pile, i) => {
+    pile.cards.forEach((card, j) => {
+      t.deepEqual(card, serializedGame.tableau.piles[i].cards[j]);
+    });
+  });
+
+  // validate the foundation
+  Object.keys(serializedGame.foundation).forEach((suit, i) => {
+    serializedGame.foundation[suit].cards.forEach((card, j) => {
+      t.deepEqual(card, game.foundation.serialize()[suit].cards[j]);
+    });
+  });
+});
+
+// // hard to test as the game state is random, we could test the history?
+// test.only("serializing returns the anticipated state", t => {
+//   const game = new KlondikeGame();
+
+//   game.start();
+
+//   const
+// });
--- engine/src/classes/KlondikeGame.ts
+++ engine/src/classes/KlondikeGame.ts
@@ -1,9 +1,9 @@
 import { ISerializable } from "../types/ISerializable";
 import { Card } from "./Card";
-import { ISerializedFoundation } from "./Foundation";
-import { ISerializedTableau } from "./Tableau";
-import { ISerializedWaste } from "./Waste";
-import { ISerializedStock } from "./Stock";
+import { ISerializedFoundation, Foundation } from "./Foundation";
+import { ISerializedTableau, Tableau } from "./Tableau";
+import { ISerializedWaste, Waste } from "./Waste";
+import { ISerializedStock, Stock } from "./Stock";
 
 type GameLocation = "tableau" | "foundation" | "waste" | "stock";
 
@@ -43,13 +43,38 @@ export interface IKlondikeGame
    * Puts all the cards back into the stock and shuffles.
    */
   reset(): void;
+  history: IMove[];
+  tableau: Tableau;
+  foundation: Foundation;
+  waste: Waste;
+  stock: Stock;
 }
 
 export class KlondikeGame implements IKlondikeGame {
+  public history: IMove[] = [];
+  public tableau: Tableau;
+  public foundation: Foundation;
+  public waste: Waste;
+  public stock: Stock;
+
   serialize(): ISerializedKlondikeGame {
-    return null;
+    return {
+      foundation: this.foundation.serialize(),
+      history: this.history,
+      stock: this.stock.serialize(),
+      tableau: this.tableau.serialize(),
+      waste: this.waste.serialize()
+    };
   }
-  static unserialize(serializedGame: ISerializedKlondikeGame) {}
+  static unserialize(serializedGame: ISerializedKlondikeGame): KlondikeGame {
+    const game = new KlondikeGame();
+    game.foundation = Foundation.unserialize(serializedGame.foundation);
+    game.tableau = Tableau.unserialize(serializedGame.tableau);
+    game.waste = Waste.unserialize(serializedGame.waste);
+    game.stock = Stock.unserialize(serializedGame.stock);
+    return game;
+  }
+
   start() {}
   reset() {}
   validateMove(move: IMove): boolean {
